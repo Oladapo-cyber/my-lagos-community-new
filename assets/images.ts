a1902b875a7d30ab39da725848ad83e1b1219afc
daps <ydapo50@gmail.com>
2026-02-11 13:34:53 +0100

    feat: add asset files (images)

---
+++ assets/images.ts
@@ -0,0 +1,6 @@
+
+export const IMAGES = {
+  TESTIMONIAL_BG: 'https://communitycra.vercel.app/static/media/testimonial-bg.c8ade2e3b78e6414590c.png',
+  HERO_BG: 'https://images.unsplash.com/photo-1599951684347-152e00c3b879?q=80&w=2000&auto=format&fit=crop',
+  CAFE_BG: 'https://images.unsplash.com/photo-1554118811-1e0d58224f24?q=80&w=2000&auto=format&fit=crop'
+};
